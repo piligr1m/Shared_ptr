21c27ea6ad0528f8d73abbdccc122daf91b8d5af
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2019-10-28 00:50:54 +0300

    Create main.cpp

---
+++ sources/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <atomic>
+#include "shared_ptr.h"
+using namespace std;
+
+int main(){
+
+    Shared_Ptr<int> ptr(10);
+    Shared_Ptr<int> ptr1(ptr);
+
+    cout << "ptr count(2)" << ptr.use_count() << endl;
+    cout << "ptr count(2)" << ptr1.use_count() << endl;
+
+    Shared_Ptr<int> ptr2(ptr1);
+
+    cout << "ptr count(3)" << ptr.use_count() << endl;
+    cout << "ptr count(3)" << ptr1.use_count() << endl;
+
+    ptr.reset();
+    ptr2.reset();
+
+    cout << "ptr count(0)" << ptr.use_count() << endl;
+    cout << "ptr count(1)" << ptr1.use_count() << endl;
+    cout << "ptr count(0)" << ptr2.use_count() << endl;
+
+    Shared_Ptr<int> ptr3(15);
+
+    ptr3.swap(ptr1);
+    cout << endl;
+    cout << ptr.use_count() << endl;
+    cout << ptr1.use_count() << endl;
+    return 0;
+}
